c793ae6ad0e9048973d4e03a714fd5eecfb60ac5
R4ndF <ronialfredo15@gmail.com>
2023-02-06 11:44:55 +0700

    roni

---
+++ dashboard/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+    <div class="container w3-display-topmiddle">
+<div class="w3-bar w3-green ">
+  <div class="w3-bar-item">home</div>
+  <div class="w3-bar-item">Pembayaran</div>
+  <div class="w3-bar-item">riwayat</div>
+</div>
+    </div>
+<div class="w3-bar-block w3-green w3-display-topleft">
+
+  <div class="w3-bar-item">siswa</div>
+  <div class="w3-bar-item">logout</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/manual.css">
+</head>
+<body>
+<div class="w3-container ">
+  <div class=" w3-display-middle w3-round ">
+    <div class="">
+      <h2 class="w3-black w3-border-aqua w3-round-large">Pembayaran Spp</h2>
+    </div>
+    <br>
+    <form action="proses/proses_login.php" method="post">
+      <p>
+      <input class="w3-input w3-round-xxlarge w3-black w3-border-aqua " type="text" placeholder="Username">
+      </p>
+      <p>    
+      <input class="w3-input w3-round-xxlarge w3-black w3-border-aqua" type="password" placeholder="Password">
+      </p>
+      <br>
+      <button class="w3-button w3-round-xlarge w3-hover-aqua w3-black w3-border w3-border-aqua w3-right">Login</button>
+    </form>
+  </div>
+</div>
+</body>
+</html>
